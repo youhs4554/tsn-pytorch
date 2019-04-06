0cd6ccc10756531e265ce0e2f869bfc834fa5c06
Rohan M <rohanmahadev7@gmail.com>
2019-04-06 14:19:27 +0000

    added i3d

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        target = target.cuda(async=True)
+        target = target.cuda()
         input_var = torch.autograd.Variable(input)
         target_var = torch.autograd.Variable(target)
 
@@ -210,7 +210,7 @@ def validate(val_loader, model, criterion, iter, logger=None):
 
     end = time.time()
     for i, (input, target) in enumerate(val_loader):
-        target = target.cuda(async=True)
+        target = target.cuda()
         input_var = torch.autograd.Variable(input, volatile=True)
         target_var = torch.autograd.Variable(target, volatile=True)
 
